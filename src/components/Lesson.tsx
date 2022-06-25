96b4ee2128be119760d159c8d030520ade2edb55
RodrigoNiederauer <rodrigonied@hotmail.com>
2022-06-25 17:42:25 -0300

    first commit

---
+++ src/components/Lesson.tsx
@@ -0,0 +1,65 @@
+import { CheckCircle, Lock } from 'phosphor-react';
+import { isPast, format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import { Link, useParams } from 'react-router-dom';
+import classNames from 'classnames';
+
+interface LessonProps {
+  title: string;
+  slug: string;
+  availableAt: Date;
+  type: 'live' | 'class';
+}
+
+export function Lesson(props: LessonProps) {
+  const { slug } = useParams<{slug: string}>()
+
+  const isCurrentLesson = slug === props.slug;
+
+  const isLessonAvailable = isPast(props.availableAt);
+  const availableDateFormatted = format(props.availableAt, "EEEE' • 'd' de 'MMMM' • 'k'h'mm", {
+    locale: ptBR,
+  });
+
+  return (
+    <Link to={`/event/lesson/${props.slug}`} className='group'>
+      <span className="text-gray-300">
+        {availableDateFormatted}
+      </span>
+
+      <div className={classNames("rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500", {
+        'bg-green-500': isCurrentLesson,
+      })}>
+        <header className="flex items-center justify-between">
+
+          {isLessonAvailable ? (
+            <span className={classNames("flex items-center gap-2 text-sm text-blue-500 font-medium", {
+              'text-white': isCurrentLesson
+            })}>
+              <CheckCircle size={20}/>
+              Conteúdo liberado
+            </span>
+
+          ): (
+            <span className="flex items-center gap-2 text-sm text-orange-500 font-medium">
+              <Lock size={20}/>
+              Em breve
+            </span>
+          )}
+
+          <span className={classNames("text-xs rounded px-2 py-[0.125rem] text-white border border-green-300 font-bold", {
+            'border-white': isCurrentLesson,
+          })}>
+            {props.type === 'live' ? 'AO VIVO' : 'AULA PRÁTICA'}
+          </span>
+        </header>
+        <strong className={classNames('mt-5 block', {
+          'text-white': isCurrentLesson,
+          'text-gray-200': !isCurrentLesson
+        })}>
+          {props.title}
+        </strong>
+      </div>
+    </Link>
+  )
+}
